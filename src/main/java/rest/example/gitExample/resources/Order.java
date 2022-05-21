3adb9a6983dec6a141cb37c5efeebe3dfdde0a5c
Renos Bardis <renosbardis@gmail.com>
2022-05-21 12:28:26 +0200

    adding entities for the 2 resources

--- src/main/java/rest/example/gitExample/resources/Order.java
+++ src/main/java/rest/example/gitExample/resources/Order.java
@@ -1,4 +1,34 @@
 package rest.example.gitExample.resources;
 
-public class Order {
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@Table(name = "orders")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "order_id", updatable = false, nullable = false)
+    private Integer orderId;
+    @NotNull
+    @Column(name = "product_name")
+    private String productName;
+    @NotNull
+    @Column(name = "order_type")
+    private String orderType;
+    @NotNull
+    @Column(name = "price")
+    private String price;
+    @NotNull
+    @Column(name = "category")
+    private String category;
 }
